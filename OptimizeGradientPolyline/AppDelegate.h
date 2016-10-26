c24e71524a2b7424c45e4a9850da3ee159824e8a
Song Xiaoguang <xgsong@mobvoi.com>
2016-10-26 16:38:35 +0800

    Initial Commit

---
+++ OptimizeGradientPolyline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OptimizeGradientPolyline
+//
+//  Created by 宋晓光 on 26/10/2016.
+//  Copyright © 2016 Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OptimizeGradientPolyline/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OptimizeGradientPolyline
+//
+//  Created by 宋晓光 on 26/10/2016.
+//  Copyright © 2016 Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
